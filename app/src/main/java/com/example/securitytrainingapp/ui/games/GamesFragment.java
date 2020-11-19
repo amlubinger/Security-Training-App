5625229b97f7054e4c65632e7c2f1a5e864761dd
Andrew Lubinger <45952290+amlubinger@users.noreply.github.com>
2020-11-18 23:03:48 -0500

    Added navigation. (To the available games along with the back button.)

--- app/src/main/java/com/example/securitytrainingapp/ui/games/GamesFragment.java
+++ app/src/main/java/com/example/securitytrainingapp/ui/games/GamesFragment.java
@@ -8,6 +8,7 @@ import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 
 import com.example.securitytrainingapp.R;
@@ -17,13 +18,41 @@ public class GamesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_best_password, container, false);
-        PasswordCreator passwordCreatorNoCapsNums = new PasswordCreator(10, true, false, false);
-        PasswordCreator passwordCreatorCapsNums = new PasswordCreator(20, true, false, true);
-        final Button button1 = (Button)view.findViewById(R.id.passwordButtonTop);
-        final Button button2 = (Button)view.findViewById(R.id.passwordButtonBottom);
-        button1.setText(passwordCreatorNoCapsNums.newPassword());
-        button2.setText(passwordCreatorCapsNums.newPassword());
+        View view = inflater.inflate(R.layout.fragment_games, container, false);
+//        PasswordCreator passwordCreatorNoCapsNums = new PasswordCreator(10, true, false, false);
+//        PasswordCreator passwordCreatorCapsNums = new PasswordCreator(20, true, false, true);
+//        final Button button1 = (Button)view.findViewById(R.id.passwordButtonTop);
+//        final Button button2 = (Button)view.findViewById(R.id.passwordButtonBottom);
+//        button1.setText(passwordCreatorNoCapsNums.newPassword());
+//        button2.setText(passwordCreatorCapsNums.newPassword());
+        Button passwordChooser = view.findViewById(R.id.button_pwrd_chooser);
+        Button phishing = view.findViewById(R.id.button_phishing_challenge);
+        Button mfa = view.findViewById(R.id.button_mfa_quiz);
+        Button decryption = view.findViewById(R.id.button_decryption_challenge);
+        passwordChooser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.playPasswordGame);
+            }
+        });
+        phishing.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        mfa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.playMFAGame);
+            }
+        });
+        decryption.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
         return view;
     }
 }
