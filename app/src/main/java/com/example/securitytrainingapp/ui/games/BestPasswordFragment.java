b09ae70a9b36be6765611050531c33e52d7d7183
Eli Vosniak <eli.vosniak@gmail.com>
2020-11-23 12:44:57 -0500

    Refactored the password game a little bit to add the difficulty buttons

--- app/src/main/java/com/example/securitytrainingapp/ui/games/BestPasswordFragment.java
+++ app/src/main/java/com/example/securitytrainingapp/ui/games/BestPasswordFragment.java
@@ -12,7 +12,6 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.securitytrainingapp.R;
-import com.example.securitytrainingapp.games.util.PasswordCreator;
 import com.example.securitytrainingapp.games.util.PasswordGame;
 
 import java.util.List;
@@ -24,6 +23,9 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
     private PasswordGame gameController;
     private Button topButton;
     private Button bottomButton;
+    private Button difficulty1Button;
+    private Button difficulty2Button;
+    private Button difficulty3Button;
     private TextView scoreDisplayText;
     private TextView instructions;
     private Random rand;
@@ -42,25 +44,19 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_best_password, container, false);
-        /*
-          TODO: Might want to have difficulty selection buttons at the top, but only available based on user profile
-          (must have certain number of points to select difficulty two and three)
-         */
+        difficulty1Button = view.findViewById(R.id.difficultyButton1);
+        difficulty2Button = view.findViewById(R.id.difficultyButton2);
+        difficulty3Button = view.findViewById(R.id.difficultyButton3);
         topButton = view.findViewById(R.id.passwordButtonTop);
         bottomButton = view.findViewById(R.id.passwordButtonBottom);
         scoreDisplayText = view.findViewById(R.id.textScoreDisplay);
         instructions = view.findViewById(R.id.instructions);
-        //Set up buttons with text
-        List<String> passwords = gameController.getPasswords();
-        if (isTopButtonCorrect) {
-            topButton.setText(passwords.get(1));
-            bottomButton.setText(passwords.get(0));
-        } else {
-            topButton.setText(passwords.get(0));
-            bottomButton.setText(passwords.get(1));
-        }
+        setPasswordButtonsDisabled();
         topButton.setOnClickListener(this);
         bottomButton.setOnClickListener(this);
+        difficulty1Button.setOnClickListener(this);
+        difficulty2Button.setOnClickListener(this);
+        difficulty3Button.setOnClickListener(this);
 
         return view;
     }
@@ -73,6 +69,24 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
                 break;
             case R.id.passwordButtonBottom:
                 onBottomButtonClick(view);
+                break;
+            case R.id.difficultyButton1:
+                gameController = new PasswordGame(5, 1);
+                setPasswordButtonsText();
+                setPasswordButtonsEnabled();
+                instructions.setText("Select the more secure password!");
+                break;
+            case R.id.difficultyButton2:
+                gameController = new PasswordGame(6, 2);
+                setPasswordButtonsText();
+                setPasswordButtonsEnabled();
+                instructions.setText("Select the more secure password!");
+                break;
+            case R.id.difficultyButton3:
+                gameController = new PasswordGame(8, 3);
+                setPasswordButtonsText();
+                setPasswordButtonsEnabled();
+                instructions.setText("Select the more secure password!");
                 break;
         }
     }
@@ -98,8 +112,7 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
             }
         } else {
             //Set text view to show score
-            instructions.setVisibility(View.INVISIBLE);
-            scoreDisplayText.setText("You scored: " + gameController.percentageCorrect() + "%");
+            instructions.setText("You scored: " + gameController.percentageCorrect() + "%");
             //Disable buttons
             topButton.setEnabled(false);
             bottomButton.setEnabled(false);
@@ -128,8 +141,7 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
             }
         } else {
             //Set text view to show score
-            instructions.setVisibility(View.INVISIBLE);
-            scoreDisplayText.setText("You scored: " + gameController.percentageCorrect() + "%");
+            instructions.setText("You scored: " + gameController.percentageCorrect() + "%");
             //Disable buttons
             topButton.setEnabled(false);
             bottomButton.setEnabled(false);
@@ -139,5 +151,26 @@ public class BestPasswordFragment extends Fragment implements View.OnClickListen
 
     private boolean isTopButtonCorrect() {
         return rand.nextBoolean();
+    }
+
+    private void setPasswordButtonsEnabled() {
+        topButton.setEnabled(true);
+        bottomButton.setEnabled(true);
+    }
+    private void setPasswordButtonsDisabled() {
+        topButton.setEnabled(false);
+        bottomButton.setEnabled(false);
+    }
+
+    private void setPasswordButtonsText() {
+        //Set up buttons with text
+        List<String> passwords = gameController.getPasswords();
+        if (isTopButtonCorrect) {
+            topButton.setText(passwords.get(1));
+            bottomButton.setText(passwords.get(0));
+        } else {
+            topButton.setText(passwords.get(0));
+            bottomButton.setText(passwords.get(1));
+        }
     }
 }
