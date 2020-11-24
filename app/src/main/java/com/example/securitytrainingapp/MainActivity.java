f971892e544d3d7ddf63171189e40cf2d0b75844
Caleb Goddard <goddard.103@osu.edu>
2020-11-23 22:18:37 -0500

    Added more rewards

--- app/src/main/java/com/example/securitytrainingapp/MainActivity.java
+++ app/src/main/java/com/example/securitytrainingapp/MainActivity.java
@@ -51,7 +51,14 @@ public class MainActivity extends AppCompatActivity {
         alertDialog.show();
 
         //Create the rewards list.
-        rewards.add(new Reward("OSU T-Shirt", 750));
+        rewards.add(new Reward("Buckeye Necklace", 750));
+        rewards.add(new Reward("Brutus Keychain", 1000));
+        rewards.add(new Reward("OSU T-Shirt", 1250));
+        rewards.add(new Reward("Football Tickets", 1500));
+        rewards.add(new Reward("Desk Chair", 1750));
+        rewards.add(new Reward("Laptop", 2000));
+        rewards.add(new Reward("Signed Heisman Trophy", 3000));
+
     }
 
     @Override
